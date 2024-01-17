6f715ef0d98a05fa27382512a045897337dc19b7
asytyi <alexander.a.sytyi@gmail.com>
2024-01-17 16:29:37 +0300

    Отключена блокировка WD

--- Firmware/Sources/Main.c
+++ Firmware/Sources/Main.c
@@ -86,7 +86,6 @@ void main()
 		// Set watch-dog as WDRST
 		ZwSystem_SelectDogFunc(FALSE);
 		ZwSystem_EnableDog(SYS_WD_PRESCALER);
-		ZwSystem_LockDog();
 
 		// Start timers
 		ZwTimer_StartT0();
